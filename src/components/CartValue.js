ac7d22f8ededed2573d4d16a729b26ea875923d2
Qin Geng <gengqin50@gmail.com>
2024-07-02 09:10:40 -0700

    shopping app

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -2,6 +2,13 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const { CartValue, Location } = useContext(AppContext);
+    
+    return (
+        <div className='alert alert-primary'>
+            <span>Cart Value: {Location}{CartValue}</span>
+        </div>
+    );
 };
 
 export default CartValue;
